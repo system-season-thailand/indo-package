1d3fcde7f09865965d068ed159bf83a7b687ba74
Season Travel <system.seasonthailand@gmail.com>
2026-02-08 19:30:57 +0700

    Update all-company-and-hotel-names.js

--- all-company-and-hotel-names.js
+++ all-company-and-hotel-names.js
@@ -298,6 +298,8 @@ const companyNames = [
     { name: "رحلة العالم", company_by_value: "awa" },
     { name: "xml holiday", company_by_value: "mst" },
     { name: "طلعة للسفر", company_by_value: "ofs" },
+    { name: "نيو ريفر", company_by_value: "ofs" },
+    { name: "طير تورز", company_by_value: null },
     { name: "واجهة السفر", company_by_value: null },
     { name: "رحلة", company_by_value: null },
     { name: "اميال المتحدة", company_by_value: null },
